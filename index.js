f0ad8b7bc90e89d22c99f152b92011936a13dfed
donotfalter-lab <hjamyll@yahoo.com>
2026-07-09 00:34:21 -0400

    functionslab2.0

--- index.js
+++ index.js
@@ -18,5 +18,8 @@ function isPalindrome(word) {
   return word === reversed;
 }
 
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  return originalPrice - (originalPrice * (discountPercentage / 100));
+}
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
